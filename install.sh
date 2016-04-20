a9530014d88b73806b7df5d7afff86a71da8c731
Peder Bergebakken Sundt <pbsds@hotmail.com>
2016-04-20 23:09:18 +0200

    2retarded4u

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-chmod a+x kodiremote.py
-chmod a+x kodix.py
-
 cd AsyncCLI
 python setup.py install
 cd ..
 
 cp kodiremote.py /usr/local/bin/kodiremote
 cp kodix.py /usr/local/bin/kodix
+
+chmod a+rx /usr/local/bin/kodiremote
+chmod a+rx /usr/local/bin/kodix
