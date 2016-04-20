593fe1f0875d8734466d58979cb423fc8ebdd90a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2016-04-20 23:30:11 +0200

    This is becoming kinda pretty...

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-cd AsyncCLI
+echo "Make sure you run this as root!"
+echo "sudo sh install.sh"
+
+cd AsyncCLI/
 python setup.py install
 cd ..
 
+echo "Installing kodiremote..."
 cp kodiremote.py /usr/local/bin/kodiremote
-cp kodix.py /usr/local/bin/kodix
-
 chmod a+rx /usr/local/bin/kodiremote
+
+echo "Installing kodix..."
+cp kodix.py /usr/local/bin/kodix
 chmod a+rx /usr/local/bin/kodix
